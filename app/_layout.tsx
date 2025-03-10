e80576e7b43a3f5164386a3e38991a12def3e725
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2025-03-10 14:50:08 +0900

    feat: api url 설정 및 navigator 설정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,8 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
-
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
@@ -16,7 +17,7 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-
+  const Stack = createNativeStackNavigator();
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
@@ -29,9 +30,11 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(signup)" options={{ headerShown: false }} />
-      </Stack>
+        <Stack.Navigator initialRouteName="(login)">
+          <Stack.Screen name="(signup)" options={{ headerShown: false }} component={require('./(signup)').default}/>
+          <Stack.Screen name="(login)" options={{ headerShown: false }} component={require('./(login)').default}/>
+          <Stack.Screen name="(home)" options={{ headerShown: false }} component={require('./(home)').default}/>
+        </Stack.Navigator>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
