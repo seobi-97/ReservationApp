671fe4cc2c478d1fd829c74f7349a7d64fdf393f
임경섭 <>
2025-08-07 10:33:17 +0900

    feat: reserve url 수정

--- apis/api.tsx
+++ apis/api.tsx
@@ -1,83 +1,119 @@
-import { axiosInstance } from './axiosInstance';
+import { axiosInstance } from "./axiosInstance";
 
 export const login = async (email: string, password: string) => {
-    try {
-        const response = await axiosInstance.post(`/auth/login`, { email, password });
-        return response;
-    } catch (error) {
-        console.error('Login failed:', error);
-        throw error;
-    }
+  try {
+    const response = await axiosInstance.post(`/auth/login`, {
+      email,
+      password,
+    });
+    return response;
+  } catch (error) {
+    console.error("Login failed:", error);
+    throw error;
+  }
 };
 
 export const signup = async (name: string, email: string, password: string) => {
-    try {
-        const response = await axiosInstance.post(`/auth/signup`, { name, email, password });
-        return response;
-    } catch (error) {
-        console.error('Signup failed:', error);
-        throw error;
-    }
+  try {
+    const response = await axiosInstance.post(`/auth/signup`, {
+      name,
+      email,
+      password,
+    });
+    return response;
+  } catch (error) {
+    console.error("Signup failed:", error);
+    throw error;
+  }
 };
 
 export const logout = async (user_id: number) => {
-    try {
-        console.log(user_id);
-        const response = await axiosInstance.post(`/auth/logout`, { user_id: user_id });
-        return response;
-    } catch (error) {
-        console.error('Logout failed:', error);
-        throw error;
-    }
+  try {
+    console.log(user_id);
+    const response = await axiosInstance.post(`/auth/logout`, {
+      user_id: user_id,
+    });
+    return response;
+  } catch (error) {
+    console.error("Logout failed:", error);
+    throw error;
+  }
 };
 
 export const getList = async (id?: number) => {
-    try {
-        const response = await axiosInstance.get(`/list/${id}`);
-        return response;
-    } catch (error) {
-        console.error('Get list failed:', error);
-        throw error;
-    }
+  try {
+    const response = await axiosInstance.get(`/list/${id}`);
+    return response;
+  } catch (error) {
+    console.error("Get list failed:", error);
+    throw error;
+  }
 };
 
 export const getClasses = async () => {
-    try {
-        const response = await axiosInstance.get(`/class/list`);
-        return response;
-    } catch (error) {
-        console.error('Get classes failed:', error);
-        throw error;
-    }
+  try {
+    const response = await axiosInstance.get(`/class/list`);
+    return response;
+  } catch (error) {
+    console.error("Get classes failed:", error);
+    throw error;
+  }
 };
 
-export const createClass = async (title: string, creator_id: number, start_date: string, description: string, status: string, capacity: number) => {
-    try {
-        const response = await axiosInstance.post(`/class/create`, { title, creator_id, start_date, description, status, capacity });
-        return response;
-    } catch (error) {
-        console.error('Create class failed:', error);
-        throw error;
-    }
+export const getClassesByDate = async (date: string) => {
+  try {
+    const response = await axiosInstance.get(`/class/list?date=${date}`);
+    return response;
+  } catch (error) {
+    console.error("Get classes by date failed:", error);
+    throw error;
+  }
+};
+
+export const createClass = async (
+  title: string,
+  creator_id: number,
+  start_date: string,
+  description: string,
+  status: string,
+  capacity: number
+) => {
+  try {
+    const response = await axiosInstance.post(`/class/create`, {
+      title,
+      creator_id,
+      start_date,
+      description,
+      status,
+      capacity,
+    });
+    return response;
+  } catch (error) {
+    console.error("Create class failed:", error);
+    throw error;
+  }
 };
 
 export const reserveClass = async (class_id: number, user_id: number) => {
-    try {
-        const response = await axiosInstance.post(`/class/reserve`, { class_id, user_id });
-        return response;
-    } catch (error) {
-        console.error('Reserve class failed:', error);
-        throw error;
-    }
+  try {
+    const response = await axiosInstance.post(`/reserve`, {
+      class_id,
+      user_id,
+    });
+    return response;
+  } catch (error) {
+    console.error("Reserve class failed:", error);
+    throw error;
+  }
 };
 
 // 401 에러 시, 토큰 검증
 export const checkToken = async (id: number) => {
-    try {
-        const response = await axiosInstance.post(`/auth/token`, { id: id });
-        return response;
-    } catch (error) {
-        console.error('Check token failed:', error);
-        throw error;
-    }
+  try {
+    const response = await axiosInstance.post(`/auth/token`, { id: id });
+    return response;
+  } catch (error) {
+    console.error("Check token failed:", error);
+    throw error;
+  }
 };
--- apis/axiosInstance.ts
+++ apis/axiosInstance.ts
@@ -1,99 +1,108 @@
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const API_URL = 'http://192.168.0.113:5500';
+const API_URL = "http://192.168.0.124:5500";
 
 export const axiosInstance = axios.create({
-    baseURL: API_URL,
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: API_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // 요청 인터셉터
 axiosInstance.interceptors.request.use(
-    async (config) => {
-        try {
-            const refreshToken = await AsyncStorage.getItem('refreshToken');
-            console.log('refreshToken:: ', refreshToken);
-            if (refreshToken) {
-                config.headers.Authorization = `Bearer ${refreshToken}`;
-            }
-        } catch (error) {
-            console.error('토큰 가져오기 실패:', error);
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  async (config) => {
+    try {
+      const refreshToken = await AsyncStorage.getItem("refreshToken");
+      console.log("refreshToken:: ", refreshToken);
+      if (refreshToken) {
+        config.headers.Authorization = `Bearer ${refreshToken}`;
+      }
+    } catch (error) {
+      console.error("토큰 가져오기 실패:", error);
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 // 응답 인터셉터
 axiosInstance.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    async (error) => {
-        console.log('error:: ', error);
-        const originalRequest = error.config;
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    console.log("error:: ", error);
+    const originalRequest = error.config;
 
-        // 토큰 갱신 요청이거나 로그인 요청인 경우 인터셉터 제외
-        if (originalRequest.url?.includes('/auth/token') || originalRequest.url?.includes('/auth/login') || originalRequest.url?.includes('/auth/signup')) {
-            return Promise.reject(error);
-        }
-
-        // 401 에러이고 재시도하지 않은 경우에만 처리
-        if (error.response?.status === 401 && !originalRequest._retry) {
-            originalRequest._retry = true;
-
-            try {
-                const refreshToken = await AsyncStorage.getItem('refreshToken');
-                console.log('리프레시 토큰:', refreshToken);
-
-                if (refreshToken) {
-                    // 토큰 갱신 요청 시 인터셉터 제외를 위한 플래그 추가
-                    const refreshResponse = await axios.post(
-                        `${API_URL}/auth/token`,
-                        {
-                            refreshToken: refreshToken,
-                        },
-                        {
-                            withCredentials: true,
-                            headers: {
-                                'Content-Type': 'application/json',
-                            },
-                        }
-                    );
-                    
-                
-                    console.log('토큰 갱신 성공:', refreshResponse.data);
-
-                    // 새로운 토큰 저장
-                    await AsyncStorage.setItem('accessToken', refreshResponse.data.accessToken);
-                    await AsyncStorage.setItem('refreshToken', refreshResponse.data.refreshToken);
-
-                    // 원래 요청에 새로운 토큰 설정
-                    originalRequest.headers.Authorization = `Bearer ${refreshResponse.data.accessToken}`;
-                    return axiosInstance(originalRequest);
-                } else {
-                    console.log('리프레시 토큰이 없음');
-                    // 리프레시 토큰이 없으면 로그아웃 처리
-                    await AsyncStorage.removeItem('accessToken');
-                    await AsyncStorage.removeItem('refreshToken');
-                    await AsyncStorage.removeItem('user');
-                    return Promise.reject(new Error('No refresh token available'));
-                }
-            } catch (refreshError) {
-                console.error('토큰 갱신 실패:', refreshError);
-                // 리프레시 토큰도 만료된 경우 로그아웃 처리
-                // await AsyncStorage.removeItem('accessToken');
-                // await AsyncStorage.removeItem('refreshToken');
-                // await AsyncStorage.removeItem('user');
-                return Promise.reject(refreshError);
-            }
-        }
-        return Promise.reject(error);
+    // 토큰 갱신 요청이거나 로그인 요청인 경우 인터셉터 제외
+    if (
+      originalRequest.url?.includes("/auth/token") ||
+      originalRequest.url?.includes("/auth/login") ||
+      originalRequest.url?.includes("/auth/signup")
+    ) {
+      return Promise.reject(error);
     }
+
+    // 401 에러이고 재시도하지 않은 경우에만 처리
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const refreshToken = await AsyncStorage.getItem("refreshToken");
+        console.log("리프레시 토큰:", refreshToken);
+
+        if (refreshToken) {
+          // 토큰 갱신 요청 시 인터셉터 제외를 위한 플래그 추가
+          const refreshResponse = await axios.post(
+            `${API_URL}/auth/token`,
+            {
+              refreshToken: refreshToken,
+            },
+            {
+              withCredentials: true,
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+
+          console.log("토큰 갱신 성공:", refreshResponse.data);
+
+          // 새로운 토큰 저장
+          await AsyncStorage.setItem(
+            "accessToken",
+            refreshResponse.data.accessToken
+          );
+          await AsyncStorage.setItem(
+            "refreshToken",
+            refreshResponse.data.refreshToken
+          );
+
+          // 원래 요청에 새로운 토큰 설정
+          originalRequest.headers.Authorization = `Bearer ${refreshResponse.data.accessToken}`;
+          return axiosInstance(originalRequest);
+        } else {
+          console.log("리프레시 토큰이 없음");
+          // 리프레시 토큰이 없으면 로그아웃 처리
+          await AsyncStorage.removeItem("accessToken");
+          await AsyncStorage.removeItem("refreshToken");
+          await AsyncStorage.removeItem("user");
+          return Promise.reject(new Error("No refresh token available"));
+        }
+      } catch (refreshError) {
+        console.error("토큰 갱신 실패:", refreshError);
+        // 리프레시 토큰도 만료된 경우 로그아웃 처리
+        // await AsyncStorage.removeItem('accessToken');
+        // await AsyncStorage.removeItem('refreshToken');
+        // await AsyncStorage.removeItem('user');
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
 );
